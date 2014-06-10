3deb1ac9bbd8eb06ec2dc77c2ec52ed0a8868f9d
Nic McPhee <mcphee@morris.umn.edu>
2014-06-09 23:33:11 -0500

    Inlined the alive_in_next_generation methods
    The alive_in_next_generation methods in LivingCell and DeadCell were now just one-line calls to one-line methods that really didn't need to be kept separate anymore. I also found naming the helper methods awkward at the time, which was a sign that getting rid of them was probably a reasonable thing.

--- lib/GameOfLife/living_cell.rb
+++ lib/GameOfLife/living_cell.rb
@@ -1,10 +1,6 @@
 class LivingCell
   
   def alive_in_next_generation
-    stable_neighborhood?
-  end
-  
-  def stable_neighborhood?
     number_of_neighbors == 2 || number_of_neighbors == 3
   end
   
