13c3213300bbbcf0be92e42ff5124545e8888e1b
Nic McPhee <mcphee@morris.umn.edu>
2014-06-09 21:25:11 -0500

    Revised world tests to use new World#empty method.

--- test/test_world.rb
+++ test/test_world.rb
@@ -6,32 +6,33 @@ require_relative "../lib/GameOfLife/location"
 class TestWorld < Test::Unit::TestCase
   
   def setup
-    @world = World.new
+    @empty_world = World.empty
     @location = Location.new(1, 1)
   end
 
-  def test_a_new_world_is_empty
-    assert @world.empty?
+  def test_an_empty_world_is_empty
+    assert @empty_world.empty?
   end
   
   def test_an_empty_world_stays_empty_after_a_tick
-    world = World.empty
-    next_world = world.tick
+    next_world = @empty_world.tick
     assert next_world.empty?
   end
   
   def test_a_new_world_does_not_have_a_cell
-    assert (not @world.alive_at?(@location))
+    assert (not @empty_world.alive_at?(@location))
   end
   
   def test_a_cell_can_be_added_to_the_world
-    @world.set_living_at(@location)
-    assert @world.alive_at?(@location)
+    world = World.empty
+    world.set_living_at(@location)
+    assert world.alive_at?(@location)
   end
   
   def test_after_adding_a_cell_the_world_is_not_empty
-    @world.set_living_at(@location)
-    assert (not @world.empty?)
+    world = World.empty
+    world.set_living_at(@location)
+    assert (not world.empty?)
   end
 
 end
